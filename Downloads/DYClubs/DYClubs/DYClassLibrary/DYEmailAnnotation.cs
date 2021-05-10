53e0760103100f2d44516d60f12aa6ea084ef4c6
Dong Yan <dyan4970@conestogac.on.ca>
2021-05-10 00:20:35 -0400

    initial commit

---
+++ Downloads/DYClubs/DYClubs/DYClassLibrary/DYEmailAnnotation.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DYClassLibrary
+{
+    public class DYEmailAnnotation : ValidationAttribute
+    {
+        public DYEmailAnnotation()
+        {
+            ErrorMessage = "{0} is invalid";
+        }
+        /// <summary>
+        /// customer annotation to validate email
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="validationContext"></param>
+        /// <returns></returns>
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string emailRegx = @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+            Regex rg = new Regex(emailRegx);
+
+            if (value == null || value.ToString() == "")
+            {
+                return ValidationResult.Success;
+            }
+            else if (rg.IsMatch(value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult(string.Format(ErrorMessage, validationContext.DisplayName));
+            }
+        }
+
+
+    }
+}
+
